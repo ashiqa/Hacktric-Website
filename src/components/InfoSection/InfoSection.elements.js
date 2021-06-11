2cfb8e9b8486278c96ce88f300e977eca8264a64
ashiqa <rahmanashiqa@gmail.com>
2021-06-11 22:20:36 +0300

    font changed

--- src/components/InfoSection/InfoSection.elements.js
+++ src/components/InfoSection/InfoSection.elements.js
@@ -75,7 +75,7 @@ export const Heading = styled.h1`
 export const Subtitle = styled.p`
   max-width: 440px;
   margin-bottom: 35px;
-  font-size: 22px;
+  font-size: 18px;
   line-height: 24px;
   color: ${({ lightTextDesc }) => (lightTextDesc ? '#a9b3c1' : '#1c2237')};
 `;
--- src/globalStyles.js
+++ src/globalStyles.js
@@ -6,7 +6,7 @@ const GlobalStyle = createGlobalStyle`
   box-sizing: border-box;
   margin: 0;
   padding: 0;
-  font-family: 'Fira Sans', sans-serif;
+  font-family: 'Source Sans Pro', sans-serif;
  } 
 `;
 
